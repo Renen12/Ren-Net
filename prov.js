b17ed507c1069bbdc5150190a014ebf9a284be0c
Olle Modin <ollemodin@outlook.com>
2024-05-18 09:13:15 +0200

    Lade till provdelning, etc

--- prov.js
+++ prov.js
@@ -1,10 +1,13 @@
 let frågorochsvar = {};
-
+let laddningsframgång = 0
 function onnewquestion() {
     let fråga = document.getElementById("fråga-indata").value;
     let nyfrågaelement = document.createElement("p");
     nyfrågaelement.innerText = fråga;
-
+    if (fråga.trim() == "" ) {
+        alert("Du kan inte lägga till en tom fråga.")
+        return
+    }
     // Use a unique id for each question element
     let questionId = "nyfråga-" + Date.now();
     nyfrågaelement.id = questionId;
@@ -31,7 +34,36 @@ function onexamstart() {
             frågorochsvar[questionElement.innerText] = answerElement.value;
         }
     });
+    if (Object.keys(frågorochsvar).length <= 0) {
+        alert("Du kan inte fylla i ett tomt prov.")
+        return
+    }
     // Convert the object to a JSON string before storing it in localStorage
     localStorage.setItem("frågorochsvar", JSON.stringify(frågorochsvar));
     window.location = "prov-efterifyllning.html";
+}
+document.getElementById("dela").onclick = () => {
+    let questionElements = document.querySelectorAll('p[id^="nyfråga-"]');
+    questionElements.forEach(questionElement => {
+        let questionId = questionElement.id;
+        let answerId = "nyfrågasvar-" + questionId.split('-')[1];
+        let answerElement = document.getElementById(answerId);
+        if (answerElement) {
+            frågorochsvar[questionElement.innerText] = answerElement.value;
+        }
+    });
+    if (Object.keys(frågorochsvar).length <= 0) {
+        alert("Du kan inte fylla i ett tomt prov.")
+        return
+    }
+    navigator.clipboard.writeText(JSON.stringify(frågorochsvar))
+}
+document.getElementById("ladda").onclick = () => {
+    document.getElementById("laddatext").style.display = "block"
+    document.getElementById("ladda").innerText = "Ladda"
+    laddningsframgång++
+    if (laddningsframgång === 2) {
+        localStorage.setItem("frågorochsvar", document.getElementById("laddatext").value)
+        window.location = "prov-efterifyllning.html"
+    }
 }
