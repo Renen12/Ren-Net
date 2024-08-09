6956b85cb438e24751feb0dcabc8a88a3fb33b76
Renen12 <ollemodin@outlook.com>
2024-08-09 21:16:31 +0200

    changed some naming

---
+++ betyg.js
@@ -0,0 +1,57 @@
+function räknaut() {
+    let allafelaktiga = false
+    let hkk = document.getElementById("hkk")
+    let no = document.getElementById("no")
+    let so = document.getElementById("so")
+    let eng = document.getElementById("eng")
+    let idh = document.getElementById("idh")
+    let ma = document.getElementById("ma")
+    let språk = document.getElementById("språk")
+    let bl = document.getElementById("bl")
+    let sl = document.getElementById("sl")
+    let sv = document.getElementById("sv")
+    const alla = [hkk.value, no.value, so.value, eng.value, idh.value, ma.value, språk.value, bl.value, sl.value, sv.value]
+    let medelvärde = 0;
+    let felaktigabetyg = 0;
+    alla.forEach(function(värde) {
+       try {
+        switch (värde.toUpperCase()) {
+            case "a".toUpperCase():
+                medelvärde += 1
+                break
+            case "b".toUpperCase() :
+                medelvärde += 2
+                break
+            case "c".toUpperCase():
+                medelvärde += 3
+                break
+            case "d".toUpperCase():
+                medelvärde += 4
+                break
+            case "e".toUpperCase():
+                medelvärde += 5
+                break
+            case "f".toUpperCase():
+                medelvärde += 0
+                break
+            default:
+                felaktigabetyg += 1
+                break
+        }
+       } catch (error) {
+        console.log(error)
+       }
+    });
+    if (felaktigabetyg == alla.length) {
+        alert("Du måste fylla i något betyg.")
+        allafelaktiga = true
+    }
+    if (felaktigabetyg > 0 && allafelaktiga == false) {
+        alert(`Du har ${felaktigabetyg} felaktiga betyg! Felaktiga betyg inkluderas inte i medelvärdet.`)
+    }
+    if (allafelaktiga != true) {
+        const uträknatmedelvärde = Math.round(medelvärde / alla.length)
+        localStorage.setItem("uträknatmedelvärde", uträknatmedelvärde)
+        window.location = "resultat.html"
+    }
+}
--- resultat.js
+++ resultat.js
@@ -1,47 +1,46 @@
-// lägg till milan yazdanfar bild och vine boom
-window.onload = function() {
-    let betyg = document.getElementById("betyg")
-    let se = document.getElementById("se")
-    let march = document.getElementById("march")
-    document.body.onmousedown = () => {
-        if (document.getElementById("se") != null) {
-            march.play()
-        }
+window.onload = function () {
+  let betyg = document.getElementById("betyg");
+  let se = document.getElementById("se");
+  let march = document.getElementById("march");
+  document.body.onmousedown = () => {
+    if (document.getElementById("se") != null) {
+      march.play();
     }
-    se.onclick = () => {
-        se.remove()
-        march.pause()
-        switch (parseInt(localStorage.getItem("uträknatmedelvärde"))) {
-            case 0:
-                betyg.innerText = "Du fick ett F."
-                new Audio("vine-boom.mp3").play()
-                setTimeout(() => {
-                    new Audio("milan.mp3").play()
-                }, 1000);
-                let img = document.getElementById("milan")
-                img.src = "milan.jpg"
-                break
-            case 1:
-                betyg.innerText = "Du fick ett A."
-                let yipee = document.getElementById("yipee").src = "yipee.webm"
-                yipee.play()
-                break
-            case 2:
-                betyg.innerText = "Du fick ett B."
-                break
-            case 3:
-                betyg.innerText = "Du fick ett C."
-                break
-            case 4:
-                betyg.innerText = "Du fick ett D."
-                break
-            case 5:
-                betyg.innerText = "Du fick ett E."
-                break
-            default:
-                alert("Vad ska jag säga")
-                window.close()
-                break
-        }
+  };
+  se.onclick = () => {
+    se.remove();
+    march.pause();
+    switch (parseInt(localStorage.getItem("uträknatmedelvärde"))) {
+      case 0:
+        betyg.innerText = "Du fick ett F.";
+        new Audio("vine-boom.mp3").play();
+        setTimeout(() => {
+          new Audio("milan.mp3").play();
+        }, 1000);
+        let img = document.getElementById("milan");
+        img.src = "milan.jpg";
+        break;
+      case 1:
+        betyg.innerText = "Du fick ett A.";
+        let yipee = (document.getElementById("yipee").src = "yipee.webm");
+        yipee.play();
+        break;
+      case 2:
+        betyg.innerText = "Du fick ett B.";
+        break;
+      case 3:
+        betyg.innerText = "Du fick ett C.";
+        break;
+      case 4:
+        betyg.innerText = "Du fick ett D.";
+        break;
+      case 5:
+        betyg.innerText = "Du fick ett E.";
+        break;
+      default:
+        alert("Vad ska jag säga");
+        window.close();
+        break;
     }
-}
+  };
+};
