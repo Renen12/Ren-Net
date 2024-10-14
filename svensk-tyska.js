5d3d371592c3e2d0ba66e8ae2abd89f5ce7d578f
Olle Modin <ollemodin@outlook.com>
2024-10-14 18:18:31 +0200

    Mer förbättringar för rovfågel och tyskaprovet. Lade också till uppgifter inför det nya provet!

--- svensk-tyska.js
+++ svensk-tyska.js
@@ -1,24 +1,9 @@
-// ändra om det kommer något till prov
 document.body.style.backgroundColor = "#808080"
-let nothing = true
 if (nothing) {
     window.location = "finnsingenting.html"
 }
 document.getElementById("byt").onclick = () => {
     window.location = "tyska.html"
-}
-let frågorochsvar = {
-    "die Schule": "skolan",
-    "die Schulfächer": "skolämnen",
-    "die Kunst": "konsten, bildämnet",
-    "Geschichte": "historia",
-    "Erdkunde": "geografi",
-    "die Stunde": "timmen, lektionen",
-    "Zwillinge": "tvillingar",
-    "Warum nicht?": "Varför inte?",
-    "die Affenhitze": "apvärmen, hettan",
-    "der Eingang": "ingången",
-    "Bis dann!": "Då ses vi då!",
 }
 let fråga = document.getElementById("fråga")
 let index = 1
@@ -26,7 +11,7 @@ let svar = document.getElementById("svar")
 let svara = document.getElementById("svara")
 let felsvar = 1;
 fråga.innerText = `Fråga 1: ${Object.values(frågorochsvar)[0]}`
-document.getElementById("svara").onclick = () => {
+function answer() {
     let svenskasvaret = Object.keys(frågorochsvar)[index - 1 ]
     if ( /* tyska svaret */Object.keys(frågorochsvar)[index - 1 ].toLowerCase().trim() == /* Användarens svar: */ svar.value.toLowerCase().trim()) {
             // orkar inte ändra
@@ -49,4 +34,12 @@ document.getElementById("svara").onclick = () => {
     fråga.innerText = `Fråga ${index + 1}: ${Object.values(frågorochsvar)[index]}`
     index++;
     svar.value = ""
+}
+document.getElementById("svara").onclick = () => {
+    answer();
+}
+document.getElementById("svar").onkeydown = (k) => {
+    if (k.keyCode == 13) {
+        answer();
+    }
 }
--- tyska.js
+++ tyska.js
@@ -1,24 +1,33 @@
 document.body.style.backgroundColor = "#808080"
-// ändra om det kommer något till prov
-let nothing = true
 if (nothing) {
     window.location = "finnsingenting.html"
 }
 document.getElementById("byt").onclick = () => {
     window.location = "svensk-tyska.html"
 }
-let frågorochsvar = {
-    "die Schule": "skolan",
-    "die Schulfächer": "skolämnen",
-    "die Kunst": "konsten, bildämnet",
-    "Geschichte": "historia",
-    "Erdkunde": "geografi",
-    "die Stunde": "timmen, lektionen",
-    "Zwillinge": "tvillingar",
-    "Warum nicht?": "Varför inte?",
-    "die Affenhitze": "apvärmen, hettan",
-    "der Eingang": "ingången",
-    "Bis dann!": "Då ses vi då!",
+function answer() {
+    let svenskasvaret = frågorochsvar[Object.keys(frågorochsvar)[index - 1]]
+    if ( /* Svenska svaret */frågorochsvar[Object.keys(frågorochsvar)[index - 1]].toLowerCase().trim() == /* Användarens svar: */ svar.value.toLowerCase().trim()) {
+        // orkar inte ändra
+    } else {
+        if (felsvar == 3) {
+            alert(`Fel svar, det rätta svaret var ${svenskasvaret}`)
+            felsvar = 1
+            return
+
+        }
+        alert("Fel svar, försök igen.")
+        felsvar++;
+        return
+
+    }
+    if (index + 1 > Object.keys(frågorochsvar).length) {
+        alert("Du har svarat på alla frågor.")
+        window.location = "index.html"
+    }
+    fråga.innerText = `Fråga ${index + 1}: ${Object.keys(frågorochsvar)[index]}`
+    index++;
+    svar.value = ""
 }
 let fråga = document.getElementById("fråga")
 let index = 1
@@ -27,26 +36,10 @@ let svara = document.getElementById("svara")
 let felsvar = 1;
 fråga.innerText = `Fråga 1: ${Object.keys(frågorochsvar)[0]}`
 document.getElementById("svara").onclick = () => {
-    let svenskasvaret = frågorochsvar[Object.keys(frågorochsvar)[index - 1 ]]
-    if ( /* Svenska svaret */frågorochsvar[Object.keys(frågorochsvar)[index - 1 ]].toLowerCase().trim() == /* Användarens svar: */ svar.value.toLowerCase().trim()) {
-            // orkar inte ändra
-    } else {
-        if (felsvar == 3) {
-            alert(`Fel svar, det rätta svaret var ${svenskasvaret}`)
-            felsvar = 1
-            return
-            
-        }
-        alert("Fel svar, försök igen.")
-        felsvar++;
-        return
-
+    answer();
+}
+document.getElementById("svar").onkeydown = (k) => {
+    if (k.keyCode == 13) {
+        answer();
     }
-    if (index +1 > Object.keys(frågorochsvar).length) {
-        alert("Du har svarat på alla frågor.")
-        window.location = "index.html"
-    }
-    fråga.innerText = `Fråga ${index + 1}: ${Object.keys(frågorochsvar)[index]}`
-    index++;
-    svar.value = ""
 }
