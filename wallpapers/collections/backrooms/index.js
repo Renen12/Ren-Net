fdadac51b464cb77fe5019a23fb3734a90154ea0
Olle Modin <ollemodin@outlook.com>
2024-12-03 08:07:34 +0100

    Added the actual wallpapers into the code

--- wallpapers/collections/backrooms/index.js
+++ wallpapers/collections/backrooms/index.js
@@ -1,4 +1,5 @@
-let fileNames = [0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 24, 25, 69, "A safe place"];
+let fileNames = [0, 10, 11, 12, 13, 14, 15, 17, 1, 24, 25, 2, 37, 389, 38, 3, 40, 4, 5, 69, 6, 7, 8, 9, 'A safe place'
+];
 fileNames.forEach((fileName) => {
     let imgElem = document.createElement("img")
     imgElem.src = fileName + ".png";
