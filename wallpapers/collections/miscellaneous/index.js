5ce00136fb3d0ba35f041e58c330f2e14a735e97
Olle Modin <ollemodin@outlook.com>
2024-12-08 15:00:12 +0100

    Why did that not apply?

---
+++ wallpapers/collections/miscellaneous/index.js
@@ -0,0 +1,21 @@
+let fileNames = [
+    "error",
+]
+
+
+fileNames.forEach((fileName) => {
+    let imgElem = document.createElement("img")
+    imgElem.src = fileName + ".png";
+    imgElem.setAttribute("download", fileName + ".png")
+    imgElem.onclick = () => {
+        let tmpLink = document.createElement("a")
+        tmpLink.download = ""
+        tmpLink.id = "tmp"
+        tmpLink.href = fileName + ".png"
+        document.body.appendChild(tmpLink)
+        document.getElementById("tmp").click();
+        document.getElementById("tmp").remove();
+    }
+    imgElem.style.userSelect = "none"
+    document.body.appendChild(imgElem);
+});
