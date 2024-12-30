2c75cab8820c999e657187bf9d0df2a3bd9bdd41
Olle Modin <ollemodin@outlook.com>
2024-12-30 20:39:37 +0100

    Fixes

--- renworks/myFile.js
+++ renworks/myFile.js
@@ -1,6 +1,5 @@
 import { createNewElementWithAttributes } from "./renWorks.js";
 let referenceForLater = createNewElementWithAttributes("h2", {
-    centered: true,
     class: "external"
 }, "renWorks exports some useful functions such as createElementWithAttributes and $(elementId).");
 // referenceForLater.innerText = "Modified"
