bd069c622ad2ecd0bdb9857d08dcf37f3d19193f
Olle Modin <ollemodin@outlook.com>
2025-01-02 18:32:57 +0100

    Clock format changes

--- modern.js
+++ modern.js
@@ -3,14 +3,10 @@ function update() {
     let date = new Date();
     let hour = date.getHours();
     let minute = date.getMinutes();
-    let second = date.getSeconds();
     if (minute.toString().length == 1) {
         minute = "0" + minute;
     }
-    if (second.toString().length == 1) {
-        second = "0" + second;
-    }
-    let time = `${hour}:${minute}:${second}`
+    let time = `${hour}:${minute}`
     $("time-text").innerText = time;
 }
 setInterval(() => {
