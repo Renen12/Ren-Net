979a06eabdd5112ac432d7fd404b933b4b140ef6
Olle Modin <ollemodin@outlook.com>
2025-01-13 18:20:45 +0100

    Lade till bloggar

---
+++ blogg/blogg.js
@@ -0,0 +1,16 @@
+import {$, loadStyleMacros} from "/renworks/renWorks.js"
+const url = "https://ren-net.pockethost.io"
+const pb = new PocketBase(url);
+window.onload = async () => {
+    let id = localStorage.getItem("post");
+    if (id == null || id === "") {
+        window.location = "index.html"
+    }
+    let richText = document.createElement("p");
+    let record = await pb.collection("bloggar").getOne(id);
+    richText.innerHTML = record.text;
+    let textDiv = $("text");
+    richText.setAttribute("flex-centered", "true");
+    textDiv.appendChild(richText)
+    loadStyleMacros()
+}
---
+++ blogg/index.js
@@ -0,0 +1,25 @@
+async function main() {
+    const url = "https://ren-net.pockethost.io";
+    const pb = new PocketBase(url)
+    const bloggar = await pb.collection("bloggar").getFullList({
+        sort: "created"
+    })
+    for (let i = 0; i < bloggar.length; i++) {
+        let post = bloggar[i];
+        let surroundingDiv = document.createElement("div");
+        surroundingDiv.className = "postDiv"
+        let button = document.createElement("button")
+        button.className = "post"
+        button.innerText = post.title
+        button.onclick = () => {
+            let id = post.id;
+            localStorage.setItem("post", id);
+            window.location = `blogg.html`
+        }
+        surroundingDiv.appendChild(button);
+        document.body.appendChild(surroundingDiv)
+    }
+}
+main().then(() => {
+    console.log("Finished Loading")
+})
