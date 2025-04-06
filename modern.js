1abfb3d6c864b1146f4da3ad935a4297a11ce469
Olle Modin <ollemodin@outlook.com>
2025-04-06 14:10:05 +0200

    Lade till veckovisning på huvudsidan

--- modern.js
+++ modern.js
@@ -1,5 +1,16 @@
 import { $ } from "https://ren-net.net/renworks/renWorks.js";
 function update() {
+    Date.prototype.getWeek = function () {
+        var target  = new Date(this.valueOf());
+        var dayNr   = (this.getDay() + 6) % 7;
+        target.setDate(target.getDate() - dayNr + 3);
+        var firstThursday = target.valueOf();
+        target.setMonth(0, 1);
+        if (target.getDay() != 4) {
+            target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+        }
+        return 1 + Math.ceil((firstThursday - target) / 604800000);
+    }    
     let date = new Date();
     let hour = date.getHours();
     let minute = date.getMinutes();
@@ -7,7 +18,7 @@ function update() {
         minute = "0" + minute;
     }
     let time = `${hour}:${minute}`
-    $("time-text").innerText = time;
+    $("time-text").innerText = `Klockan ${time}, vecka ${date.getWeek()}`;
 }
 setInterval(() => {
     update();
