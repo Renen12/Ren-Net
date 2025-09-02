78f9b71ea77236867109efe7c28d165b78c99cea
Renen12 <112264908+Renen12@users.noreply.github.com>
2025-09-02 08:05:28 +0200

    Fixa pebbes stavfel

--- tyska-frågorochsvar.js
+++ tyska-frågorochsvar.js
@@ -1,13 +1,13 @@
 let nothing = false;
 let frågorochsvar = {
   "Hur går det för dig?": "Wie geht's dir?",
-  "Att vandra är långtråkigt.": "Wandern ist langweilig.",
-  "Det var inget särskilt.": "Das war nichts Besonderes.",
+  "Att vandra är långtråkigt.": "Wandern ist langweilig",
+  "Det var inget särskilt.": "Das war nichts Besonderes",
   "Vi har fortfarande lov.": "Wir haben noch Ferien",
-  "Peter hade en cykelolycka.": "Peter hatte einen Radunfall.",
+  "Peter hade en cykelolycka.": "Peter hatte einen Fahrradunfall",
   "Kanske kommer hon nästa vecka.": "Vielleicht kom sie nächste Woche.",
   "Nu är huset färdigt.": "Jetzt ist das Haus fertig.",
-  "För två veckor sedan var vi i Ulm.": "Vor zwei Wochen waren wir in Ulm.",
+  "För två veckor sedan var vi i Ulm.": "Vor zwei Wochen waren wir in Ulm",
   "Jag måste absolut träffa din katt.": "Ich muss unbedingt deine Katze treffen"
 };
 
