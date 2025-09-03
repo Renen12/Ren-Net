369f01686039afd7731605c3fb742d2a82efa541
Renen12 <112264908+Renen12@users.noreply.github.com>
2025-09-03 09:19:49 +0200

    Update tyska-frågorochsvar.js

--- tyska-frågorochsvar.js
+++ tyska-frågorochsvar.js
@@ -5,7 +5,7 @@ let frågorochsvar = {
   "Det var inget särskilt.": "Das war nichts Besonderes",
   "Vi har fortfarande lov.": "Wir haben noch Ferien",
   "Peter hade en cykelolycka.": "Peter hatte einen Fahrradunfall",
-  "Kanske kommer hon nästa vecka.": "Vielleicht kom sie nächste Woche.",
+  "Kanske kommer hon nästa vecka.": "Vielleicht kommt sie nächste Woche.",
   "Nu är huset färdigt.": "Jetzt ist das Haus fertig.",
   "För två veckor sedan var vi i Ulm.": "Vor zwei Wochen waren wir in Ulm",
   "Jag måste absolut träffa din katt.": "Ich muss unbedingt deine Katze treffen"
